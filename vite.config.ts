c02b85097eeef7e73e8a170f1da8d7f0262a9ba7
João Vitor Rosado <joaovrosado2003@gmail.com>
2026-05-30 00:11:04 -0300

    Configura GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
         return path.resolve(__dirname, 'src/assets', filename)
@@ -17,20 +16,23 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  // Necessário para o GitHub Pages
+  base: '/KORE/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+
+    // Os plugins React e Tailwind são necessários
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // Tipos de arquivos permitidos para importação
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
